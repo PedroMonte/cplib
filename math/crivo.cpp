54ad62c399fd7842ff9c76bf4ee65c419ad631d5
PedroMonte <56232588+PedroMonte@users.noreply.github.com>
2024-03-29 22:09:29 -0300

    Update crivo.cpp

--- math/crivo.cpp
+++ math/crivo.cpp
@@ -1,3 +1,5 @@
+#include "../base.cpp"
+
 // Sieve
 const int MXN = 1e6+5;
 bitset<MXN> isPrime;
@@ -5,10 +7,11 @@ vector<int> sieve(){
 	isPrime.set();
 	isPrime[0] = 0;
 	isPrime[1] = 0;
+
 	vector<int> primes;
-	for(int i = 2; i < MXN; i++){
+	for(int i = 2; i*i < MXN; i++){
 		if(isPrime[i]){
-			primes.PB(i);
+			primes.push_back(i);
 			for(int j = 2*i; j < MXN; j += i){
 				isPrime[j] = 0;
 			}
